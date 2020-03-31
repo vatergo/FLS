fa88d0acb81a3f370449af891ffb1924954074f0
vatergo <vatergo@mail.ru>
2020-03-31 11:00:12 +0500

    template

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express, { json } from 'express';
+import path from 'path';
+
+//const port = process.env.PORT;
+const port = config.get('port');
+
+const app = express();
+
+app.use(json());
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Сервер запущен на порту: ${port}`));
