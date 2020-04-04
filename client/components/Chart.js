7ee43a2e167048e4d933fa9584cbd43928406117
vatergo <vatergo@mail.ru>
2020-04-04 19:49:19 +0500

    ResponsiveChart

--- client/components/Chart.js
+++ client/components/Chart.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
 } from 'recharts';
 
 class Chart extends Component {
     render() {
         const { data, xaxisKey, barKey, color } = this.props;
         return (
-            <BarChart
-                width={1024}
-                height={300}
-                data={data}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey={xaxisKey} />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey={barKey} fill={color} />
-            </BarChart>
+            <ResponsiveContainer width='100%' height={300}>
+                <BarChart
+                    data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey={xaxisKey} />
+                    <YAxis label={{ value: 'License Usage', offset: 0, angle: -90, position: 'insideLeft' }} />
+                    <Tooltip />
+                    <Legend verticalAlign="top" height={36}/>
+                    <Bar dataKey={barKey} fill={color} />
+                </BarChart>
+            </ResponsiveContainer>
         );
     }
 }
